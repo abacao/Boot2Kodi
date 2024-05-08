d18f30089174eba1eb60291c84a3c56ef93bf2a2
Andre Bacao <17479246+abacao@users.noreply.github.com>
2024-05-08 12:13:15 +0100

    added powermenu to installation file in 24.04

--- install.sh
+++ install.sh
@@ -25,8 +25,11 @@ echo "needs_root_rights=yes" >> /etc/X11/Xwrapper.config
 # CP kodi.service to the correct place
 cp kodi.service /etc/systemd/system
 
-# CP powermenu_in_kodi.pkla to the correct place
+# CP powermenu_in_kodi.pkla for Ubuntu 22.04 and under:
 cp powermenu_in_kodi.pkla /etc/polkit-1/localauthority/50-local.d
+
+# CP powermenu_in_kodi.pkla for Ubuntu 24.04:
+cp powermenu_in_2404.rules /etc/polkit-1/rules.d/powermenu.rules
 
 # Start Kodi on boot
 systemctl enable kodi
